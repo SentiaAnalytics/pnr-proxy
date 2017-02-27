694042d4156c9c6cd4794e276b763940a6514ecf
Andreas Møller <andreas.moller@gmail.com>
2017-02-27 13:56:40 +0000

    added assertions for env vars

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 //@flow weak
+const assert = require('assert')
+assert(process.env.PORT, 'You must set the env var PORT')
+assert(process.env.PORT, 'You must set the env var PORT')
+assert(process.env.CPR_USER_ID, 'You must set the env var CPR_USER_ID')
+assert(process.env.CPR_USERNAME, 'You must set the env var CPR_USERNAME')
+assert(process.env.CPR_PASSWORD, 'You must set the env var CPR_PASSWORD')
+assert(process.env.CPR_HOST, 'You must set the env var CPR_HOST')
+
 const express = require('express')
 const app = express()
 
 const {PORT, AUTH_TOKEN} = process.env
-
 const OPTIONS = {
   userID: process.env.CPR_USER_ID,
   username: process.env.CPR_USERNAME,
